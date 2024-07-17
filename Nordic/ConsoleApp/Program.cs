931d18bf0de5cef33c97013284533f02046c09e7
Bannikov Sergey <sbannikov@croc.ru>
2024-07-17 22:05:14 +0300

    Лекция 02 - пример неявного приведения типов

--- Nordic/ConsoleApp/Program.cs
+++ Nordic/ConsoleApp/Program.cs
@@ -7,7 +7,13 @@ Console.WriteLine("Привет, вселенная!");
 int a = 10;
 a = a + 1;
 a += 1;
-int b = a++;
+
+int b = ++a;
+/*
+a = a + 1;
+int b = a;
+*/
+
 int c = ++a;
 a++;
 ++a;
@@ -45,4 +51,25 @@ d1 = double.PositiveInfinity;
 d2 = 1;
 Console.OutputEncoding = Encoding.UTF8;
 Console.WriteLine(d1);
+
+Console.Write("Введите вещественное число > ");
+string? s = Console.ReadLine();
+// d1 = double.Parse(s);
+d1 = Convert.ToDouble(s, System.Globalization.CultureInfo.InvariantCulture);
+Console.WriteLine(d1.ToString(System.Globalization.CultureInfo.InvariantCulture));
+
+int dx = (int)(d2 / 0);
+
+Console.WriteLine((int)2.999999999);
+
+int number = 55;
+char ch = 'W';
+s = "";
+string s1 = s + number; // число преобразуется в строку
+string s2 = s + ch;     // символ преобразуется в строку
+string s3 = s + (number == 55 ? number : ch); // символ преобразуется в число, а только потом число преобразуется в строку
+
+Console.WriteLine($"s1 = {s1}");
+Console.WriteLine($"s2 = {s2}");
+Console.WriteLine($"s3 = {s3}");
 
