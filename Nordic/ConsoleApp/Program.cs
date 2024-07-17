b593502de331433d0fa93128af69cbdc16c94a48
Bannikov Sergey <sbannikov@croc.ru>
2024-07-17 21:25:11 +0300

    Лекция 02 - вещественная арифметика

--- Nordic/ConsoleApp/Program.cs
+++ Nordic/ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+
+// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Привет, вселенная!");
 
@@ -22,4 +24,25 @@ Console.WriteLine(c);
 Console.WriteLine(d);
 Console.WriteLine(e);
 
+double d1 = 1, d2 = 1;
+Console.WriteLine(d1);
+d1 += 0.1;
+d1 += 0.1;
+d1 += 0.1;
+d1 += 0.1;
+d1 += 0.1;
+d2 += 0.5;
+Console.WriteLine(d1);
+Console.WriteLine(d2);
+Console.WriteLine(d1 == d2);
+
+int A = 2;
+int B = 0;
+bool flag = (B != 0) && (A / B == 1);
+Console.WriteLine(flag);
+
+d1 = double.PositiveInfinity;
+d2 = 1;
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine(d1);
 
