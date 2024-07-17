fdd3ffb258d3fe22862c43f63592649cf00da074
Bannikov Sergey <sbannikov@croc.ru>
2024-07-17 21:03:17 +0300

    Лекция 02 - целочисленная арифметика

--- Nordic/ConsoleApp/Program.cs
+++ Nordic/ConsoleApp/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Привет, вселенная!");
+
+int a = 10;
+a = a + 1;
+a += 1;
+int b = a++;
+int c = ++a;
+a++;
+++a;
+
+int d = int.MaxValue;
+int e = unchecked(d + 1);
+
+d = int.MinValue;
+e = d - 1;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+
+
